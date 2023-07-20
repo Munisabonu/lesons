472c7a6d7c71167bbf85b2eecab477f28366891a
munisabonu <muxtarovamunisabonu33@gmail.com>
2023-07-20 19:01:29 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,123 @@
+// let a = "Hello World"
+// console.log(a)
+// let c = 7
+// if (c % 2){
+//     console.log("Toq son.")
+// } else {
+//     console.log(" Juft son.")
+// }
+// let q = 10
+// if (q % 3 == 0 && q % 5 == 0) {
+//     console.log (" FizzBazz")
+// } else if (q % 5 == 0) {
+//     console.log(" Bazz")
+// } else if (q % 3 == 0) {
+//     console.log(" Fizz")
+// } else{
+//     console.log(q)
+// }
+// let w = 40
+// if (w == 100){
+//     console.log("A")
+// }else if (w < 100 && w >= 80) {
+//     console.log("B")
+// }else if (w < 80 && w >= 50){
+//     console.log("C")
+// } else { 
+// console.log("F")
+// }
+// let age = 25
+// switch (age) {
+//     case 25:
+//         console.log(" You are 25 years old ")
+//         break;
+//     case 30:
+//         console.log(" You are 30 years old ")
+//         break;
+//     default:
+//         console.log(" You are neigher 25 nor 30 years old ") ;
+//         break;   
+// }
+// let age = 60
+// switch(age){
+//     case  18:
+//         console.log("You are 18 years old and can take driver license")
+//         break;
+//     case 30: 
+//         console.log("You are 30 years old")   
+//          break;
+//      case 40:
+        // console.log(" You are 40 years old and take drever license")
+//         break;
+//      case 50:
+//         console.log("You are 50 years old and can take driver license")
+//         break;
+//     case  60:
+//         c
+// onsole.log(" You are 60 years old and can not take driver license")
+//         break;
+//     default:
+//         console.log("You can not take drever license") ;
+//         break;
+// }
+// for(i = 0; i < 100; i++)
+// console.log(i)
+// for(i = 100; i > 0; i--)
+// console.log(i)
+// let i = 15
+// if(i % 5 == 0){
+// console.log
+// }
+// for(i = 100; i > 0; i--)
+// console.log(i)
+
+
+// let car = {
+//     cost: 1000000,
+//     years: 2023
+
+// }
+//  car.cost=20000000
+//  console.log(car)
+ 
+
+// let a=0
+// for(i=1; i<=100; i++) {
+//         a+=i
+// }
+//    console.log(a)
+
+
+// let a=1
+// for(i=1; i<=100; i++) {
+//         a*=i
+// }
+//   console.log(a)
+
+
+// for(i=0; i<=100; i++) {
+//         if(i% 2!=0){
+//          console.log(i)
+//         }
+// }       
+  
+// for(i=0; i<=100; i++) {
+//         if(i % 2 ==0) {
+//         console.log(i)
+// }    
+//         }
+       
+
+// for(i=0; i<=100; i++) {
+//        if(i % 3 ==0) {
+//         console.log(i)
+// }    
+//         }
+
+// let a = 0
+// for(i=0; i<=100; i++) {
+//         if(i % 3 ==0) {
+//         a += i 
+                
+//         }    
+//                 } console.log(a)
